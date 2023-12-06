6ed7fd1adc8aa1dea1b08f1f196c92c35f176601
sharupc <arahman@idare.io>
2023-12-06 12:58:43 +0600

    react mui

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p> */}
+        <Button variant="contained">Dr Jannat Chowdhury</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
